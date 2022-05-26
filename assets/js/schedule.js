9622692fd16a28082b1b221dfb6140ffa4abf52e
emily <eap6787@gmail.com>
2022-05-26 14:22:07 -0600

    modularize

---
+++ assets/js/schedule.js
@@ -0,0 +1,7 @@
+require('bootstrap');
+const createEl = require('./domMethods');
+const { createLoremIpsum, dateConverter } = require('./helpers');
+
+$(document).ready(function () {
+  // DOM manipulation code specific to each page.
+});
